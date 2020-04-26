627d402cda6d3e9fe1dc8c56fee5006fedfbb951
asherjg1 <asherjg1@gmail.com>
2020-04-26 16:41:12 -0700

    ng

--- popup.js
+++ popup.js
@@ -1,16 +1,24 @@
 window.onload=function(){
-//getCompanies();
-alert("hek")
+getCompanies();
 }
 function getCompanies(){
-  $.ajax({ 
-    url: "http://localhost:3000/api/companies", 
-    type: 'GET', 
-    dataType: 'json', 
+$.ajax({
+  url: "http://localhost:3000/api/companies",
+  type: 'get',
+  dataType: 'json',
 
-    success: function(res) { 
-       alert("hello") 
-
-} 
-}); 
+  success: function(res){
+  getEmployees(res);
+}
+});
+}
+function getEmployees(res){
+  var tne = 0
+  var idx
+  var values=res['companies']
+  for (idx = 0; idx < values.length; idx++){
+    console.log(values[idx]['number_of_employees']);
+tne+= parseInt(values[idx]['number_of_employees'])
+}
+console.log(tne);
 }
