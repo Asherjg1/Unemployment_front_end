cbbca6809e3f64ff39cee22c3698e4ed9ad793a7
asherjg1 <asherjg1@gmail.com>
2020-05-23 12:15:40 -0700

    bing

--- popup.js
+++ popup.js
@@ -17,18 +17,18 @@ $.ajax({
 function getEmployees(res){
 
   var values=res["companies"]["total_num_laid_off"]
-$(".container.total-num-laid-off").text("Total Number Laid Off:" +values)
+$(".container.total-num-laid-off").text("Total Number Laid Off: " +values+ " Companies")
 
 }
 function getUnemploymentRate(res){
 
   var values=res["companies"]["unemployment_rate"]
-$(".container.unemployment-rate").text("Unemployment Rate: "+values)
+$(".container.unemployment-rate").text("Unemployment Rate: "+values*100+"%")
 
 }
 function getTotalCompanyLayoff(res){
 
   var values=res["companies"]["number_of_companies_laid_off"]
-$(".container.num-companies-laid-off").text("Total Number of Companies Laid Off:" +values)
+$(".container.num-companies-laid-off").text("Total Number of Companies Laid Off: " +values+ " People")
 
 }
